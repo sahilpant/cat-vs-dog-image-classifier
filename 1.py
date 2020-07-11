12e760a3f5d5e839feac097c6a203671a69fcff1
Sahil Pant <sahilpant16@hotmail.com>
2020-07-11 11:55:36 +0530

    fixed minor bugs

--- 1.py
+++ 1.py
@@ -1,5 +1,4 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import os
 import cv2
 from tqdm import tqdm
--- 2.py
+++ 2.py
@@ -32,6 +32,7 @@ model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
 
 model.add(Dense(64))
+model.add(Activation('relu'))
 
 model.add(Dense(1))
 model.add(Activation('sigmoid'))
